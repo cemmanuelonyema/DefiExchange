af9450e3a9bc80b66b9a86b508c2327c0c570e55
cemmanuelonyema <cemmanuelonyema@gmail.com>
2022-05-13 23:49:27 +0100

    Deployed

--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -1,30 +1,25 @@
-const { ethers } = require("hardhat");
+
+
+  const { ethers } = require("hardhat");
 require("dotenv").config({ path: ".env" });
-const { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS } = require("../constants");
+const { CRYPTO_DEV_TOKEN_CONTRACT_ADDRESS } = require("../constants");
 
 async function main() {
-  // Address of the Crypto Devs TOKEN contract that you deployed in the previous module
-  const cryptoDevsTokenAddress = CRYPTO_DEV_TOKEN_CONTRACT_ADDRESS;
-
+  const cryptoDevTokenAddress = CRYPTO_DEV_TOKEN_CONTRACT_ADDRESS;
   /*
-    A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
-    so cryptoDevsTokenContract here is a factory for instances of our CryptoDevToken contract.
-    */
-  const exchangeContract= await ethers.getContractFactory(
-    "Exchange"
-  );
+  A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+  so exchangeContract here is a factory for instances of our Exchange contract.
+  */
+  const exchangeContract = await ethers.getContractFactory("Exchange");
 
-  // deploy the contract
+  // here we deploy the contract
   const deployedExchangeContract = await exchangeContract.deploy(
-    cryptoDevsTokenAddress
+    cryptoDevTokenAddress
   );
   await deployedExchangeContract.deployed();
 
   // print the address of the deployed contract
-  console.log(
-    "Exchange Contract Address Deployed Successfully TO:",
-    deployedCryptoDevsTokenContract.address
-  );
+  console.log("Exchange Contract Address:", deployedExchangeContract.address);
 }
 
 // Call the main function and catch if there is any error
@@ -34,3 +29,5 @@ main()
     console.error(error);
     process.exit(1);
   });
+
+  0xdaf890929509E2E3a99fdb6C8D8B9789d4F6A627 exchange address
